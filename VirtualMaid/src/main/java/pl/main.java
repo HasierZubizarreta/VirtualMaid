adc0adfc0eb5e19f70312083eeb96f6410b6e137
janirehanze <janir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ ssh -T github.comjanir@github.com: Permission denied (publickey).janir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ ssh -v gitlab@gitlab.fk4.hs-bremen.deOpenSSH_9.0p1, OpenSSL 1.1.1q  5 Jul 2022debug1: Reading configuration data /etc/ssh/ssh_configdebug1: Connecting to gitlab.fk4.hs-bremen.de [194.94.26.194] port 22.debug1: connect to address 194.94.26.194 port 22: Connection timed outdebug1: Connecting to gitlab.fk4.hs-bremen.de [2001:638:703:13::194] port 22.debug1: connect to address 2001:638:703:13::194 port 22: Network is unreachablessh: connect to host gitlab.fk4.hs-bremen.de port 22: Network is unreachablejanir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ ssh -v gitlab@gitlab.fk4.hs-bremen.deOpenSSH_9.0p1, OpenSSL 1.1.1q  5 Jul 2022debug1: Reading configuration data /etc/ssh/ssh_configdebug1: Connecting to gitlab.fk4.hs-bremen.de [194.94.26.194] port 22.debug1: connect to address 194.94.26.194 port 22: Connection timed outdebug1: Connecting to gitlab.fk4.hs-bremen.de [2001:638:703:13::194] port 22.debug1: connect to address 2001:638:703:13::194 port 22: Network is unreachablessh: connect to host gitlab.fk4.hs-bremen.de port 22: Network is unreachablejanir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ ssh -T github.com j.pampin.rubio@st.hanze.nljanir@github.com: Permission denied (publickey).janir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ git config --global user.name janirehanzejanir@LAPTOP-JI80TM86 MINGW64 ~/Documents/University/4 maila/GROBIE/git/grobie (collect_measurements)$ git config --global user.email j.pampin.rubio@st.hazne.nl>
2023-03-21 17:27:06 +0100

    01

--- VirtualMaid/src/main/java/pl/main.java
+++ VirtualMaid/src/main/java/pl/main.java
@@ -1,13 +1,27 @@
 package pl;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String args[]) {   
 
+        int aukera = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(aukera != 0 || aukera<6){
+        
         System.out.println("Ongi etorri berriro erabiltzaile agurra.");
         System.out.println("MENU NAGUSIA:");
         System.out.println("\t00 - IRTEN\n\t01 - Iprograma berria\n\t02 - Ieguneko programak\n\t03 - Ihistoriala\n\t04 - Igailuak editatu\n");
         System.out.println("Aukera:");
+
+        aukera = scanner.nextInt();
+
+        }
+
+        System.out.println("programa amaituta");
     } 
     
 }
