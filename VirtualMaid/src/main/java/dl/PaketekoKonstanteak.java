543c4a80db52bb62a4997dfcb72db00cea28e468
967597 <967597@vbox>
2023-03-28 17:25:47 +0200

    PrezioakOrdukoB klasea

---
+++ VirtualMaid/src/main/java/dl/PaketekoKonstanteak.java
@@ -0,0 +1,5 @@
+package dl;
+
+public class PaketekoKonstanteak {
+	static final String prezioakFitxategiIzena = System.getProperty("user.home")+"/Development/Eclipse/VirtualMaid/VirtualMaid/src/main/fitxategiak/precioLuz.csv";
+}
--- VirtualMaid/src/main/java/dl/PrezioakOrdukoB.java
+++ VirtualMaid/src/main/java/dl/PrezioakOrdukoB.java
@@ -1,5 +1,56 @@
 package dl;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrezioakOrdukoB {
+	
+	public static List <Float> queryFindAllDB() {
+		List <Float> prezioak = new ArrayList<Float>();
+		String k;
+		String[] d;
+		try {
+			FileReader f = new FileReader(PaketekoKonstanteak.prezioakFitxategiIzena); 
+			BufferedReader b = new BufferedReader(f); 
+			
+			b.readLine();
+			while((k = b.readLine())!=null) { 
+				d=k.split(",");
+				prezioak.add(Float.parseFloat(d[1]));
+			} 
+			b.close(); 
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prezioak;
+	}
+	
+	public static float findPrezioa(int ordua) {
+		String k;
+		String[] d;
+		float a=0;
+		try {
+			FileReader f = new FileReader(PaketekoKonstanteak.prezioakFitxategiIzena); 
+			BufferedReader b = new BufferedReader(f); 
+			
+			for (int i=0; i<=ordua; i++) {
+				b.readLine();
+			}
+			k=b.readLine();
+			d=k.split(",");
+			a=Float.parseFloat(d[1]);
+			b.close(); 
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return a;
+	}
 
 }
