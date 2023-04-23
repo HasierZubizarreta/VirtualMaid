0005691e47058f81174964b2f36301d40d4f58f8
967552 <967552@vbox>
2023-04-23 13:06:24 +0200

    01 aukera

---
+++ VirtualMaid/src/main/java/pl/ErabiltzaileAtazakMB.java
@@ -0,0 +1,41 @@
+package pl;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import bl.ErabiltzaileaEJB;
+
+@Named
+@SessionScoped
+public class ErabiltzaileAtazakMB implements Serializable {
+	
+	@EJB private ErabiltzaileaEJB eEJB;
+	private int ordua;
+
+	public ErabiltzaileAtazakMB() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getOrdua() {
+		return ordua;
+	}
+
+	public void programaBerriaGorde(ProgramaFormMB programa){
+	    
+		eEJB.programaBerriaGorde(programa.getGailuIzena(), programa.getOrdua()); 
+	    
+	}
+	public void orduaMantendu(int ordua, PrezioAukeraketaViewMB pView) throws IOException{
+		
+		this.ordua = ordua;
+		pView.resetView();
+		FacesContext.getCurrentInstance().getExternalContext().redirect("gailuak.xhtml");
+	}
+	
+
+}
